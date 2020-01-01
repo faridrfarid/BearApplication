df8e6aecee175f6fc64a901ac49eabdef1796146
Fred <fredstone1372@gmail.com>
2020-01-01 23:50:07 +0330

    beers list , sort function , shake function

--- src/pages/Bear/BearList.js
+++ src/pages/Bear/BearList.js
@@ -7,12 +7,14 @@ import { Text, View } from 'react-native-animatable'
 import { Image, TouchableOpacity } from "react-native"
 import BearApis from '../../api/BearApis'
 import { styles } from 'BearApplication/src/styles/BearPageStyles.js'
+var sortBy = require('sort-by')
 
 class BearList extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            BeersList: []
+            BeersList: [],
+            sortFunction: 'abv'
         }
         this.bearServices = new BearApis()
     }
@@ -26,8 +28,25 @@ class BearList extends Component {
 
     UNSAFE_componentWillMount() {
         RNShake.addEventListener('ShakeEvent', () => {
-            //shake function
+            console.log('shaked')
+            this.sortFunction()
         });
+    }
+
+
+    sortFunction() {
+        if (this.state.sortFunction === 'abv') {
+            this.setState({ sortFunction: '-abv' })
+        }
+        else if (this.state.sortFunction === '-abv') {
+            this.setState({ sortFunction: 'name' })
+        }
+        else if (this.state.sortFunction === 'name') {
+            this.setState({ sortFunction: '-name' })
+        }
+        else if (this.state.sortFunction === '-name') {
+            this.setState({ sortFunction: 'abv' })
+        }
     }
 
 
@@ -40,6 +59,27 @@ class BearList extends Component {
     render() {
         return (
             <View style={styles.container}>
+                <View style={{ width: '100%', justifyContent: 'space-around', flexDirection: 'row', flexWrap: 'wrap' }}>
+                    {this.state.BeersList
+                        .sort(sortBy(this.state.sortFunction))
+                        .map((item, index) => (
+                            <View
+                                animation={"bounceInLeft"}
+                                delay={100 + index * 100}
+                                key={index} style={{ width: '30%' }}>
+                                <View style={{ height: 100, borderRadius: 10, borderColor: Param.COLOR.black, borderWidth: 1 , marginVertical : 10 }}>
+                                    <Image style={{ width: '100%', height: '100%', borderRadius: 10, resizeMode: 'cover' }}
+                                        source={{ uri: item.image_url }} />
+                                </View>
+                                <Text style={{ fontSize: 15, color: Param.COLOR.black, marginTop: 10, textAlign: 'center' }}>
+                                    {item.name}
+                                </Text>
+                                <Text style={{ fontSize: 14, color: Param.COLOR.black, marginTop: 5, textAlign: 'center' }}>
+                                    {item.abv}
+                                </Text>
+                            </View>
+                        ))}
+                </View>
             </View>
         )
     }
